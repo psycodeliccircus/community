378b2ad8d44e6e83f269b681032f7f4b12953f93
renildomarcio <krsolucoesweb@gmail.com>
2023-10-22 12:57:25 -0300

    Update 2.0.1

---
+++ config.js
@@ -0,0 +1,22 @@
+const config = {
+    // Main Application URL
+    'websiteUrl' : 'https://community.renildomarcio.com.br',
+
+    // Application Name
+    'appName' : 'Community',
+
+    // Application window width and height
+    'width' : 1280,
+    'height' : 800,
+    'minWidth' : 1280,
+    'minHeight' : 800,
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
